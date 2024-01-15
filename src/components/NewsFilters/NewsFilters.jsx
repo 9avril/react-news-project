78cc7ff5134ffea4b340742de59c51a3bb09358f
Artem <artemkiasko@gmail.com>
2024-01-15 19:28:35 +0300

    refactor code skeleton (need fix)

---
+++ src/components/NewsFilters/NewsFilters.jsx
@@ -0,0 +1,29 @@
+import styles from './NewsFilters.module.css'
+import Categories from '../Categories/Categories.jsx'
+import Search from '../Search/Search.jsx'
+import { useFetch } from '../../helpers/hooks/useFetch.js'
+import { getCategories } from '../../api/apiNews.js'
+
+const NewsFilters = ({ filters, changeFilters }) => {
+	const { data: dataCategories } = useFetch(getCategories)
+	return (
+		<div className={styles.filters}>
+			{dataCategories ? (
+				<Categories
+					categories={dataCategories.categories}
+					selectedCategory={filters.category}
+					setSelectedCategory={category =>
+						changeFilters('category', category)
+					}
+				/>
+			) : null}
+
+			<Search
+				keywords={filters.keywords}
+				setKeywords={keywords => changeFilters('keywords', keywords)}
+			/>
+		</div>
+	)
+}
+
+export default NewsFilters
--- src/components/Skeleton/Skeleton.jsx
+++ src/components/Skeleton/Skeleton.jsx
@@ -1,16 +1,38 @@
 import style from './Skeleton.module.css'
 
-const Skeleton = ({count = 1, type = 'banner'}) => {
-    return <>
-        {count > 1 ? (
-            <ul className={style.list}>
-
-                {[...Array(count).map((_, index) => {
-                    <li key={index} className={type === 'banner' ? style.banner : style.item}></li>
-                })]}
-            </ul>
-        ) : <li className={type === 'banner' ? style.banner : style.item}></li>}
-    </>
+const Skeleton = ({ count = 1, type = 'banner', direction = 'column' }) => {
+	return (
+		<>
+			{count > 1 ? (
+				<ul
+					className={
+						direction === 'column'
+							? style.columnList
+							: style.rowList
+					}
+				>
+					{[
+						...Array(count).map((_, index) => {
+							return (
+								<li
+									key={index}
+									className={
+										type === 'banner'
+											? style.banner
+											: style.item
+									}
+								></li>
+							)
+						}),
+					]}
+				</ul>
+			) : (
+				<li
+					className={type === 'banner' ? style.banner : style.item}
+				></li>
+			)}
+		</>
+	)
 }
 
 export default Skeleton
