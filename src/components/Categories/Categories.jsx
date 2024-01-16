1e20faf823124aa5a6ff964ba126b532e6cbca02
Artem <artemkiasko@gmail.com>
2024-01-16 16:20:02 +0300

    add slider

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,31 +1,36 @@
 import style from './Categories.module.css'
+import { forwardRef } from 'react'
 
-const Categories = ({ categories, setSelectedCategory, selectedCategory }) => {
-	return (
-		<div className={style.categories}>
-			<button
-				onClick={() => setSelectedCategory(null)}
-				className={!selectedCategory ? style.active : style.item}
-			>
-				All
-			</button>
-			{categories.map(category => {
-				return (
-					<button
-						onClick={() => setSelectedCategory(category)}
-						className={
-							selectedCategory === category
-								? style.active
-								: style.item
-						}
-						key={category}
-					>
-						{category}
-					</button>
-				)
-			})}
-		</div>
-	)
-}
+const Categories = forwardRef(
+	({ categories, setSelectedCategory, selectedCategory }, ref) => {
+		return (
+			<div ref={ref} className={style.categories}>
+				<button
+					onClick={() => setSelectedCategory(null)}
+					className={!selectedCategory ? style.active : style.item}
+				>
+					All
+				</button>
+				{categories.map(category => {
+					return (
+						<button
+							onClick={() => setSelectedCategory(category)}
+							className={
+								selectedCategory === category
+									? style.active
+									: style.item
+							}
+							key={category}
+						>
+							{category}
+						</button>
+					)
+				})}
+			</div>
+		)
+	},
+)
+
+Categories.displayName = 'Categories'
 
 export default Categories
